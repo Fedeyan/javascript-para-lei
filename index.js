eb585ba5a5c20bc2a80208018b1a580fdd6fd656
Federico Carusso <developer.basilorien@gmail.com>
2023-06-08 23:44:25 -0300

    initialCommit

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+ Ejercicio 1 : tipos de datos
+
+ Existen varios tipos de datos en Javascript: 
+
+ los tres principales y mas usados son: 
+ 
+ string,
+ boolean,
+ number,
+ 
+
+
+*/
+
+var string;
+var number;
+var bool;
+
+module.exports = {};
